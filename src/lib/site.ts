2d279aaeff8d909be94b34315dc329b3d4d5ce86
Omar Refaey <refa3y.dev@gmail.com>
2026-02-14 01:26:55 +0200

    update SEO++

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,2 +1,2 @@
-export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://omar-refaey-portfolio.netlify.app";
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://refa3y-portfolio.vercel.app";
 export const SITE_NAME = "Omar Ayman | Full-Stack .NET Developer";
