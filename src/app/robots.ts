9a43a694b58ae63f8e752d2825f6b557fc6539f5
Omar Refaey <refa3y.dev@gmail.com>
2026-02-14 02:22:42 +0200

    update sitemap name

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: "/",
             disallow: "/private/",
         },
-        sitemap: `${SITE_URL}/sitemap.xml`,
+        sitemap: `${SITE_URL}/sitemap-main.xml`,
     };
 }
